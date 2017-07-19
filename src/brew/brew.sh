214c910b7385f501ea8c9cfc6b4644562c94d6ec
Nathan Bailey <dev@dnbailey.org>
2017-07-18 20:11:48 -0400

    Reconfigure nvm and node install

--- src/brew/brew.sh
+++ src/brew/brew.sh
@@ -16,7 +16,13 @@ else
   brew upgrade
 
   brew install coreutils
-  brew install nvm
+
+  # Install via curl to get bash prompt
+  curl -o- https://raw.githubusercontent.com/creationix/nvm/v0.33.2/install.sh | bash
+
+  # Install Node via nvm
+  nvm install node
+
   brew cask install atom
   brew cask install github-desktop
   brew cask install fritzing
